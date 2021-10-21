ba20e999b286f77e0f5879e466ee518ca79e5c0d
rashf0rd <nguyenminhtrong12a0@gmail.com>
2021-10-21 14:22:29 +0700

    Them thu muc index.php

--- database/patient.sql
+++ database/patient.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th10 21, 2021 lúc 08:34 AM
+-- Thời gian đã tạo: Th10 21, 2021 lúc 08:50 AM
 -- Phiên bản máy phục vụ: 10.4.21-MariaDB
 -- Phiên bản PHP: 8.0.10
 
@@ -29,8 +29,8 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `patient` (
   `patientid` varchar(10) NOT NULL,
-  `firstname` varchar(50) NOT NULL,
-  `lastname` varchar(30) NOT NULL,
+  `lastname` varchar(50) NOT NULL,
+  `firstname` varchar(30) NOT NULL,
   `dateofbirth` datetime NOT NULL,
   `gender` int(1) NOT NULL,
   `mobile` int(10) NOT NULL,
@@ -46,7 +46,7 @@ CREATE TABLE `patient` (
 -- Đang đổ dữ liệu cho bảng `patient`
 --
 
-INSERT INTO `patient` (`patientid`, `firstname`, `lastname`, `dateofbirth`, `gender`, `mobile`, `email`, `height`, `weight`, `blood_type`, `created_on`, `modified_on`) VALUES
+INSERT INTO `patient` (`patientid`, `lastname`, `firstname`, `dateofbirth`, `gender`, `mobile`, `email`, `height`, `weight`, `blood_type`, `created_on`, `modified_on`) VALUES
 ('001', 'josé', 'mourinho', '1963-01-26 08:19:35', 1, 995123456, 'mou@gmail.com', 169, 65, 'O', '2013-06-15', '2013-06-22'),
 ('0594', 'macus', 'rashford', '1997-10-31 00:24:55', 1, 1515482144, 'rashMU@gmail.com', 180, 70, 'O', '2021-07-31', '2021-09-30'),
 ('1545', 'Hoàng Thị', 'Loan', '1995-02-06 08:27:22', 0, 169869158, 'L0an@gmail.com', 158, 42, 'A', '2019-12-26', '2019-10-27'),
